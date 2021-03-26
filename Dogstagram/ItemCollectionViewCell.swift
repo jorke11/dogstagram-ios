ba3ee90fb62058d85b5a3c078c995b3032af4b2b
Jeisson Monroy <jorke8710@gmail.com>
2021-03-26 08:45:59 -0500

    irst commit

---
+++ Dogstagram/ItemCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCollectionViewCell.swift
+//  Dogstagram
+//
+//  Created by Jorge Pinedo on 5/26/19.
+//  Copyright © 2019 Jorge Pinedo. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Dogstagram/ViewController.swift
+++ Dogstagram/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
+struct Item{
+    var imageName: String
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items: [Item] = [Item(imageName: "1"),Item(imageName: "2")]
+    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    
+    let cellIdentifier="ItemCollectionViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupCollectionViewItemSize()
+    }
+    
+    private func setupCollectionView(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: "ItemCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: cellIdentifier)
+    }
+    
+    private func setupCollectionViewItemSize(){
+        if collectionViewFlowLayout == nil {
+            let numberOfItemPerRow: CGFloat = 3
+            let lineSpacing: CGFloat = 5
+            let interItemSpacing: CGFloat = 5
+            
+            let width = (collectionView.frame.width - (numberOfItemPerRow - 1) * interItemSpacing) / numberOfItemPerRow
+            let height = width
+            
+            collectionViewFlowLayout = UICollectionViewFlowLayout();
+            collectionViewFlowLayout.itemSize = CGSize(width: width, height: height)
+            collectionViewFlowLayout.sectionInset = UIEdgeInsets.zero
+            collectionViewFlowLayout.scrollDirection = .vertical
+            collectionViewFlowLayout.minimumLineSpacing = lineSpacing
+            collectionViewFlowLayout.minimumInteritemSpacing = interItemSpacing
+            
+            collectionView.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
+        }
+    }
 }
 
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ItemCollectionViewCell
+       
+        print("didselectItemAt2 \(indexPath)")
+
+        cell.imageView.image = UIImage(named: items[indexPath.item].imageName)
+        return cell;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("didselectItemAt \(indexPath)")
+    }
+}
